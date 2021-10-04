2de528bcf5eb83a1083c5828ed04a0f142b77510
Jan Doležal <jandolezal@email.cz>
2021-10-04 21:02:40 +0200

    Refactor links scraper

--- links.py
+++ links.py
@@ -3,7 +3,11 @@
 """
 MPO publikuje každoročně zprávu Obnovitelné zdroje v roce RRRR se
 statistikami výroby energií z OZE.
-Script scrapuje odkazy na tyto pdf zprávy a uloží je jako csv.
+Modul pro scrapování odkazů na tyto pdf zprávy a jejich uložení ve formátu csv.
+
+year,url
+2020,https://www.mpo.cz/path/to/pdf
+...,...
 """
 
 
@@ -11,6 +15,18 @@ import re
 import csv
 import requests
 from bs4 import BeautifulSoup
+
+
+HEADERS = {
+    'user-agent': 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0'
+}
+
+BASE_URL = 'https://www.mpo.cz'
+
+START_URLS = [
+    'https://www.mpo.cz/cz/energetika/statistika/obnovitelne-zdroje-energie/',
+    'https://www.mpo.cz/cz/energetika/statistika/obnovitelne-zdroje-energie/archiv.htm',
+]
 
 
 def gather_post_urls(url, domain, headers):
@@ -22,7 +38,7 @@ def gather_post_urls(url, domain, headers):
 
     for a in a_tags_list:
         post_url_list.append(domain + a.attrs['href'])
-    
+
     return post_url_list
 
 
@@ -32,39 +48,33 @@ def gather_pdf_urls(post_url_list, domain, headers):
     for post_url in post_url_list:
         r = requests.get(post_url, headers=headers)
         bs = BeautifulSoup(r.text, 'html.parser')
-        
+
         download_url = domain + bs.find('a', {'class': 'download'}).attrs['href']
         year = re.search(re.compile('\d{4}'), post_url).group(0)
-        report_url_list.append({
-            'year': year,
-            'url': download_url,
-        })
-    
+        report_url_list.append(
+            {
+                'year': year,
+                'url': download_url,
+            }
+        )
+
     return report_url_list
 
 
-def main():
-    headers = {'user-agent': 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0'}
-    
-    domain = 'https://www.mpo.cz'
-    
-    urls = [
-        'https://www.mpo.cz/cz/energetika/statistika/obnovitelne-zdroje-energie/',
-        'https://www.mpo.cz/cz/energetika/statistika/obnovitelne-zdroje-energie/archiv.htm'
-    ]
+def pdfs_urls_to_csv(filename, urls=START_URLS, base_url=BASE_URL, headers=HEADERS):
 
     post_urls_list = []
 
     for url in urls:
-        post_urls_list += gather_post_urls(url, domain, headers)
-    
-    pdf_url_list = gather_pdf_urls(post_urls_list, domain, headers)
+        post_urls_list += gather_post_urls(url, base_url, headers)
 
-    with open('odkazy.csv', 'w', newline='') as csvf:
+    pdf_url_list = gather_pdf_urls(post_urls_list, base_url, headers)
+
+    with open(filename, 'w', newline='') as csvf:
         writer = csv.DictWriter(csvf, fieldnames=['year', 'url'])
         writer.writeheader()
         writer.writerows(pdf_url_list)
 
 
 if __name__ == '__main__':
-    main()
+    pdfs_urls_to_csv('odkazy.csv')
